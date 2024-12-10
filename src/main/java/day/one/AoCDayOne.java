3f00af20a0c4389868843c9813d07e08c1ac7542
Dinesh R N <rndinesh751@gmail.com>
2024-12-10 16:20:21 +0530

    day one: reduced loops

--- src/main/java/day/one/AoCDayOne.java
+++ src/main/java/day/one/AoCDayOne.java
@@ -1,11 +1,11 @@
 package day.one;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /*
 Trying to solve the AoC day one problem
@@ -45,32 +45,17 @@ public class AoCDayOne {
     }
 
     static Long getTotalDistance(List<Long> left, List<Long> right) {
-        long distance = 0L;
         left.sort(Long::compare);
         right.sort(Long::compare);
 
-        Iterator<Long> leftIt = left.iterator();
-        Iterator<Long> rightIt = right.iterator();
-
-        while (leftIt.hasNext() && rightIt.hasNext()) {
-            Long leftNow = leftIt.next();
-            Long rightNow = rightIt.next();
-            distance = distance + Math.abs(leftNow - rightNow);
-        }
-
-        return distance;
+        return IntStream.range(0, Math.min(left.size(), right.size()))
+                .mapToLong(i -> Math.abs(left.get(i)) - right.get(i)).sum();
     }
 
     static Long getSimilarityScore(List<Long> left, List<Long> right) {
-        Map<Long, Integer> counter = right.stream().collect(HashMap::new, (a, b) -> {
-            if (a.containsKey(b)) {
-                a.put(b, a.get(b) + 1);
-            }
-            a.putIfAbsent(b, 1);
-        }, (a, b) -> {
-        });
+        Map<Long, Long> counter = right.stream().collect(Collectors.groupingBy(num -> num, Collectors.counting()));
 
-        return left.stream().reduce(0L, (aLong, aLong2) -> aLong + aLong2 * (counter.getOrDefault(aLong2, 0)));
+        return left.stream().mapToLong(aLong2 -> aLong2 * (counter.getOrDefault(aLong2, 0L))).sum();
     }
 
 }
